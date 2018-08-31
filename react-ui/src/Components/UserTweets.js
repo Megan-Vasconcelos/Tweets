21ef3915f4edc1af843982e3cd3e4ec99f38bb91
Megan <megansvasconcelos@gmail.com>
2018-08-31 12:25:16 -0400

    switched server.js to index.js

--- react-ui/src/Components/UserTweets.js
+++ react-ui/src/Components/UserTweets.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import RenderTweet from './tweet';
+import RenderTweet from './RenderTweet';
 import {fetchTweets} from '../Action';
 
 
