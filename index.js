dceafa1a6cfee3968ea030bd4deb58afe535cb7a
Megan <megansvasconcelos@gmail.com>
2018-08-31 15:59:02 -0400

    dotenv

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const port = process.env.PORT || 5000;
 
-require('dotenv').config();
+// require('dotenv').config();
 const app = express();
 
 const client = new Twitter({
